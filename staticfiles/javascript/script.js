b8a173b2d9efcc1c8e2b595c585c80e42e50623a
Popsicool <akinolasamson1234@gmail.com>
2022-08-19 12:56:01 +0100

    'update'

--- staticfiles/javascript/script.js
+++
@@ -1,9 +0,0 @@
-const toggleBtns = document.querySelectorAll(".toggle");
-const toggleContent = document.querySelectorAll(".toggle-content");
-
-toggleBtns.forEach((toggleBtn, id) => {
-  toggleBtn.addEventListener("click", () => {
-    toggleContent[id].classList.toggle("show-faq");
-  });
-});
-
